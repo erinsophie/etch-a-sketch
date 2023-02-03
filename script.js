33e154c1dd0cb54b3903e6062bc749f30b25805f
Sophie Little <119007056+erinsophie@users.noreply.github.com>
2023-02-03 17:44:51 +0000

    Set up initial color change function

--- script.js
+++ script.js
@@ -2,13 +2,39 @@
 //This grabs the already existing grid-container div 
 
 const grid = document.querySelector('.grid-container');
+const blackPixels = document.querySelector('.black-pixels');
+const rainbowPixels = document.querySelector('.rainbow-pixels')
+
+//EVENT LISTENERS 
+
+blackPixels.addEventListener('click', changeColor);
+rainbowPixels.addEventListener('click', changeColor);
+
 
 //SET UP GRID 
 //This loop creates a 16 * 16 grid by creating a new div element each iteration and setting its class to "square" then appending that square div to the parent element grid div
 
-for (let i = 0; i < 16 * 16; i++) {
+function createGrid() {
+    for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mousedown', changeColor);
+    square.addEventListener('mouseover', changeColor);
     grid.appendChild(square);
+  }
 }
+
+createGrid()
+   
+//COLORS
+
+function changeColor() {
+   this.style.backgroundColor = 'black';
+}
+
+
+
+
+
+
 
